bd58c352733a8afe352b5808cd56869996fa7158
LyamBRS <cous5830@gmail.com>
2026-02-17 19:39:23 -0500

    Distance sensor filter strategy

---
+++ scripts/vehicle/sensors/distance/filters/distance_sensor_filter_strategy.gd
@@ -0,0 +1,13 @@
+extends RefCounted
+class_name DistanceSensorFilterStrategy
+
+## Abstract (interface) definition of a filtering strategy for the distance sensor.
+##
+## Your implementations should read, once implemented, like:
+## "low-pass.filter(distance);"
+##
+## @param raw: Unfiltered distance from a simulated or real sensor.
+## @return Filtered distance based on a chosen filtering strategy.
+func filter(raw: float) -> float:
+	push_error("filter() must be overridden! Strategy not implemented.")
+	return 0.0
---
+++ scripts/vehicle/sensors/distance/filters/no_filter.gd
@@ -0,0 +1,9 @@
+extends DistanceSensorFilterStrategy
+class_name NoFilter
+
+## Returns the raw value as is, not filtered or altered at all.
+##
+## @param raw: Unfiltered distance from a simulated or real sensor.
+## @return raw directly returned.
+func filter(raw: float) -> float:
+	return raw
