26a8ee8d1795a88dc69261cfcf18ee59bd5b776d
LyamBRS <cous5830@gmail.com>
2026-02-17 23:11:57 -0500

    Steering n stuff, for lack of a better commit

---
+++ scripts/vehicle/sensors/line/configurations/line_sensor_config.gd
@@ -0,0 +1,18 @@
+class_name LineSensorConfig
+extends RefCounted
+
+## Milimeters. Useful to understand the angle of the line.
+var distance_between_sensors: float
+## How many sensors is there on the sensor?
+var sensor_amount: int
+
+var adapter_type: String
+
+func _init(
+	distance_between_sensors=0.0,
+	sensor_amount=5,
+	adapter_type_value="none"
+):
+	self.distance_between_sensors = distance_between_sensors
+	self.sensor_amount = sensor_amount
+	self.adapter_type = adapter_type
