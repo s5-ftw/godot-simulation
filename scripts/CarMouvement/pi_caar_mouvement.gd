1304936fe601a19fabfe9718ef5a9cb82d7162cb
Franky55 <43887787+Franky55@users.noreply.github.com>
2026-03-03 12:27:26 -0500

    Car moves like a car maybe need to adjuste the speed of everithing, but they are variables

--- scripts/CarMouvement/pi_caar_mouvement.gd
+++ scripts/CarMouvement/pi_caar_mouvement.gd
@@ -1,42 +1,40 @@
 extends Node3D
 
 @export var speed = 0.0
-@export var max_speed = 14.0
-@export var acceleration = 0.1
-@export var decceleration = 0.15
+@export var max_speed = 10.0
+@export var acceleration = 5.0
+@export var decceleration = 4.0
+@export var turn_speed = 0.5
 
 func _process(delta: float) -> void:
-	var direction = Vector3(0,0,1)
-	
+	# Turning
 	if Input.is_key_pressed(KEY_A):
-		direction.x += 1
+		rotate_y(turn_speed * speed * delta)
 	if Input.is_key_pressed(KEY_D):
-		direction.x -= 1
-		
+		rotate_y(-turn_speed * speed * delta)
+	
+	# Acceleration
 	if Input.is_key_pressed(KEY_W):
-		speed -= acceleration
+		speed += acceleration * delta
 	elif Input.is_key_pressed(KEY_S):
-		speed += acceleration
+		speed -= acceleration * delta
 	else:
-		translate(slow_down_letting_go(delta))
-		return
-	
-	translate(direction * speed * delta)
+		slow_down_letting_go(delta)
+		
+	speed = clamp(speed, -max_speed, max_speed)
+		
+	# Move in the direction the car faces
+	var forward = -transform.basis.z
+	position += forward * speed * delta
 
 
-func slow_down_letting_go(delta: float) -> Vector3:
-	var direction = Vector3(0.0,0.0,1.0)
-
+func slow_down_letting_go(delta: float) -> void:
 	if speed > 0:
-		if speed <= decceleration:
+		speed -= decceleration * delta
+		if speed < 0:
 			speed = 0
-		else:
-			speed -= decceleration
-	
-	if speed < 0:
-		if abs(speed) <= decceleration:
+
+	elif speed < 0:
+		speed += decceleration * delta
+		if speed > 0:
 			speed = 0
-		else:
-			speed += decceleration
-	
-	return direction * speed * delta
