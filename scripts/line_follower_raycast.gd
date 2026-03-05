7ca30367ca105ad410b71fcf0a752d4ea428566c
LyamBRS <cous5830@gmail.com>
2026-03-05 14:48:52 -0500

    Removed test movements

--- scripts/line_follower_raycast.gd
+++ scripts/line_follower_raycast.gd
@@ -9,47 +9,39 @@ func set_line_follower_at(index: int, value: int) -> void:
 	if line_follower_array[index] != value:
 		line_follower_array[index] = value
 		emit_signal("line_follower_changed", index, line_follower_array)
-		
-
-var _initial_y: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self._initial_y = global_position.y
-	pass # Replace with function body.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta): # Use physics_process instead
-	var time = Time.get_ticks_msec() / 1000.0
-	var offset = sin(time * 2.0) * 0.5
-	global_position.y = _initial_y + offset
-	
+func _physics_process(delta):
 	for i in line_follower_array.size():
 		var ray = get_child(i)
 		if ray is RayCast3D:
 			set_line_follower_at(i, int(ray.is_colliding()))
 
-func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	print("_on_body_shape_entered")
-	if body.collision_layer == 2:
-		set_line_follower_at(local_shape_index, 1)
-	print(line_follower_array)
-
-
-func _on_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
-	print("_on_body_shape_exited")
-	if body.collision_layer == 2:
-		set_line_follower_at(local_shape_index, 0)
-
-func _on_area_entered(area: Area3D) -> void:
-	print("_on_area_entered")
-
-func _on_body_exited(body: Node3D) -> void:
-	print("_on_body_exited")
-
-func _on_area_shape_exited() -> void:
-	print("_on_area_shape_exited")
-
-func _on_area_exited(area: Area3D) -> void:
-	print("_on_area_exited")
-	pass # Replace with function body.
+# Teacher's program is kept here for the eventuality that it's required.
+#func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	#print("_on_body_shape_entered")
+	#if body.collision_layer == 2:
+		#set_line_follower_at(local_shape_index, 1)
+#
+#
+#func _on_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
+	#print("_on_body_shape_exited")
+	#if body.collision_layer == 2:
+		#set_line_follower_at(local_shape_index, 0)
+#
+#func _on_area_entered(area: Area3D) -> void:
+	#print("_on_area_entered")
+#
+#func _on_body_exited(body: Node3D) -> void:
+	#print("_on_body_exited")
+#
+#func _on_area_shape_exited() -> void:
+	#print("_on_area_shape_exited")
+#
+#func _on_area_exited(area: Area3D) -> void:
+	#print("_on_area_exited")
+	#pass
