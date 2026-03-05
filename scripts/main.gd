10f9620fd0f07e2dceb10e274edbfb003e4fd727
LyamBRS <cous5830@gmail.com>
2026-03-05 14:27:15 -0500

    Binds sensor array to adapter

--- scripts/main.gd
+++ scripts/main.gd
@@ -16,6 +16,7 @@ func _ready():
 	# Vehicle creation logic and debug assignments
 	manager = VehicleFactory.create("SunFounder PiCar")
 	manager.bind_debug_ui(debug_ui_container)
+	manager.adapters.line_sensor.bind(line_sensor_area.get_child(0))
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
