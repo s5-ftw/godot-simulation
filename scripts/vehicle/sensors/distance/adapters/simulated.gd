be08f4851d5bcc21d2f7d0d4e63584f80be0eb5e
LyamBRS <cous5830@gmail.com>
2026-03-09 12:27:27 -0400

    Additions of read raycast function

--- scripts/vehicle/sensors/distance/adapters/simulated.gd
+++ scripts/vehicle/sensors/distance/adapters/simulated.gd
@@ -5,21 +5,20 @@ extends DistanceSensorAdapter
 
 ## Keeps track of the polling interval to avoid making you think you can poll really fast.
 var next_ready_time: float = 0.0
-var bound_node: Node = null
 const SOUND_SPEED_CM_PER_SEC = 34300.0 # approx 343 m/s
 
 ## Follows precision time set in config
 ## @returns centimeters
 func read() -> float:
-	if bound_node == null:
-		push_error("SimulatedDistanceSensor is not bound to a Node!")
+	if self._bound_raycast == null:
+		push_error("SimulatedDistanceSensor is not bound to a Raycast! it cannot read a distance without a raycast.")
 		return 0.0
 
-	var distance = 10 # TODO: HARDCODED. FIX.
+	var distance = _read_raycast_distance()
 	distance = _apply_precision_noise(distance)
-	var time_to_wait = _calculate_await_time(distance)
+	#var time_to_wait = _calculate_await_time(distance)
 	## AWAIT LOGIC SIMULATING BEING STUCK WAITING FOR SENSOR ECHO
-	await bound_node.create_timer(time_to_wait).timeout
+	##await bound_node.create_timer(time_to_wait).timeout
 	
 	var now = Time.get_ticks_msec() / 1000.0
 	# You didn't wait for the poll interval to end. Your signal has higher chance to be noisy
@@ -48,3 +47,36 @@ func _calculate_await_time(distance_cm: float) -> float:
 func _apply_precision_noise(distance_cm: float) -> float:
 	var noise = randf_range(-self.config.precision, self.config.precision)
 	return distance_cm + noise
+
+func _read_raycast_distance() -> float:
+	if not self._bound_raycast.is_colliding():
+		return self.config.max_distance
+	
+	var origin: Vector3 = self._bound_raycast.global_transform.origin
+	var collision_point: Vector3 = self._bound_raycast.get_collision_point()
+	var distance_to_object = origin.distance_to(collision_point)
+	
+	if distance_to_object < self.config.min_distance:
+		distance_to_object = self.config.min_distance
+	
+	return distance_to_object
+	
+## Debug the simulated distance sensor.
+func _bind_debug_ui(container: GridContainer) -> void:
+	## Clear existing UI
+	for child in container.get_children():
+		child.queue_free()
+	
+	## Setup for centered text
+	container.columns = 1
+
+	var label = Label.new()
+	label.text = "Not done yet"
+	label.add_theme_font_size_override("font_size", 12)
+	
+	label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	label.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	
+	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	container.add_child(label)
