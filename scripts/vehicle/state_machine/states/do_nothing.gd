891716a29108ed8df501fab7fdbc4221745a000c
Franky55 <43887787+Franky55@users.noreply.github.com>
2026-03-11 16:45:15 -0400

    yo

--- scripts/vehicle/state_machine/states/do_nothing.gd
+++ scripts/vehicle/state_machine/states/do_nothing.gd
@@ -2,8 +2,6 @@
 ## Serving as a placeholder when no states were made
 class_name DoNothingState
 extends VehicleState
-
-var following: LineFollowing
 	
 ## Does nothing at all
 func execute(delta):
